f0d6d1859f61d268d1b82cc5d3adfac3a5489224
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-02-21 22:26:39 +0100

    set example on how to override min http bucket

--- src/main/java/no/skatteetaten/aurora/openshift/reference/springboot/Main.java
+++ src/main/java/no/skatteetaten/aurora/openshift/reference/springboot/Main.java
@@ -1,12 +1,25 @@
 package no.skatteetaten.aurora.openshift.reference.springboot;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.config.MeterFilter;
 
 @SpringBootApplication
 public class Main {
 
     protected Main() {
+    }
+
+    private static int MIN_MILLIS = 300;
+
+    //Example on how to override default 100 milis lower bound for buckets
+    @Bean
+    MeterFilter minExpectedHttp() {
+        return MeterFilter.minExpected("http", Duration.ofMillis(MIN_MILLIS));
     }
 
     public static void main(String[] args) throws Exception {
