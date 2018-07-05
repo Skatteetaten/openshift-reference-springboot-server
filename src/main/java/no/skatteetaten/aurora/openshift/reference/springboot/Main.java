20b59c76272a90ef98554f8258aec366ba115cb4
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-07-05 12:12:05 +0200

    fixed tests after RestTemplate include

--- src/main/java/no/skatteetaten/aurora/openshift/reference/springboot/Main.java
+++ src/main/java/no/skatteetaten/aurora/openshift/reference/springboot/Main.java
@@ -2,9 +2,6 @@ package no.skatteetaten.aurora.openshift.reference.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Main {
@@ -16,8 +13,4 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
 }
