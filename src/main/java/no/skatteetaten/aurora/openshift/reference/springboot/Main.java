f58bd80acee196d8e033b4f9ab1a6778780abe54
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-07-05 12:03:56 +0200

    added restTemplate

--- src/main/java/no/skatteetaten/aurora/openshift/reference/springboot/Main.java
+++ src/main/java/no/skatteetaten/aurora/openshift/reference/springboot/Main.java
@@ -2,6 +2,9 @@ package no.skatteetaten.aurora.openshift.reference.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Main {
@@ -11,5 +14,10 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
